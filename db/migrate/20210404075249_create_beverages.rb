c6dbf61d80a9f0c5d86cb2bbe6a83c7f14aae13e
TAKAE1117 <takayuki.ogawa.0712@gmail.com>
2021-04-04 17:22:28 +0900

    creating a beverages table

--- db/migrate/20210404075249_create_beverages.rb
+++ db/migrate/20210404075249_create_beverages.rb
@@ -1,7 +1,13 @@
 class CreateBeverages < ActiveRecord::Migration[6.0]
   def change
     create_table :beverages do |t|
-
+      t.string  :item,            null: false # 商品名
+      t.string  :milk_type,       null: false # ミルクの種類
+      t.integer :energy,          null: false # エネルギー（kcal）
+      t.integer :protein,         null: false # タンパク質（g）
+      t.integer :lipid,           null: false # 脂質（g）
+      t.integer :carbohydrate,    null: false # 炭水化物（g）
+      t.integer :salt_equivalent, null: false # 食塩相当量（g）
       t.timestamps
     end
   end
---
+++ db/schema.rb
@@ -0,0 +1,27 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_04_04_075249) do
+
+  create_table "beverages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "item", null: false
+    t.string "milk_type", null: false
+    t.integer "energy", null: false
+    t.integer "protein", null: false
+    t.integer "lipid", null: false
+    t.integer "carbohydrate", null: false
+    t.integer "salt_equivalent", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
